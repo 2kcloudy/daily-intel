b1c75ffa9634040ff2fa62d3109936a9743ebc62
2kcloudy <emailkeithnow@yahoo.com>
2026-04-20 16:27:44 -0400

    feat: health tab - app/health/[date]/page.jsx

---
+++ app/health/[date]/page.jsx
@@ -0,0 +1,20 @@
+import { getHealthDigest, getAllHealthDates } from "@/lib/storage";
+import { notFound } from "next/navigation";
+import HealthDigestView from "@/components/HealthDigestView";
+
+export const revalidate = 3600;
+
+export default async function HealthDatePage({ params }) {
+  const { date } = params;
+  const [digest, allDates] = await Promise.all([
+    getHealthDigest(date),
+    getAllHealthDates(),
+  ]);
+  if (!digest) notFound();
+  return <HealthDigestView digest={digest} allDates={allDates} />;
+}
+
+export async function generateStaticParams() {
+  const dates = await getAllHealthDates();
+  return dates.map((date) => ({ date }));
+}
